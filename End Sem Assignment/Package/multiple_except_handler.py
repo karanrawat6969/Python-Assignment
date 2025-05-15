d4e1dbbeb0722ecc24b9f701efc5bd10b01b55c7
karanrawat6969 <shankybhai49@gmail.com>
2025-05-15 07:11:55 +0530

    Delete End Sem Assignment/Package/multiple_except_handler.py

--- End Sem Assignment/Package/multiple_except_handler.py
+++
@@ -1,8 +0,0 @@
-try:
-    x = 5 / 0
-except ZeroDivisionError:
-    print("Caught ZeroDivisionError")
-except NameError:
-    print("Caught NameError")
-except Exception as e:
-    print("Caught general exception:", e)
